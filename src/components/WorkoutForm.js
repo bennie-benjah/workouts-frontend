9b908c2efb56940aeba70a99cb500ba935a7ed09
benjamin <mutisob78@gmail.com>
2025-11-04 09:51:38 +0300

    FRONTEND URL

--- src/components/WorkoutForm.js
+++ src/components/WorkoutForm.js
@@ -27,7 +27,7 @@ const WorkoutForm = () => {
     setError(null);
 
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_URL}/api/workouts`, {
+      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/api/workouts`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
